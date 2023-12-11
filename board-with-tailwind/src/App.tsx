31cb0c639bb2cc7472580374d252b34291835e76
KIM WONHEE <49189226+wooneeS2@users.noreply.github.com>
2023-12-11 16:43:54 +0900

    Update App.tsx

--- board-with-tailwind/src/App.tsx
+++ board-with-tailwind/src/App.tsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
